56c032979c8abdb87d2dde31f56b2dc966c5b4ba
CrispyPin <crispin@tasa.se>
2020-10-04 17:54:58 +0200

    press end to reload scene

--- ld-47/scripts/DebugMenu.gd
+++ ld-47/scripts/DebugMenu.gd
@@ -4,6 +4,6 @@ func _process(_delta):
 	if Input.is_action_just_pressed("ui_home"):
 		visible = !visible
 	if Input.is_action_just_pressed("ui_end"):
-		get_tree().paused = true
+		get_tree().reload_current_scene()
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
