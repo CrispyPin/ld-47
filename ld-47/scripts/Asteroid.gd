fbf432f9ae25af9e5fd50e758567ef0ae8838d86
CrispyPin <crispin@tasa.se>
2020-10-04 17:38:20 +0200

    collisions with small asteroids work

--- ld-47/scripts/Asteroid.gd
+++ ld-47/scripts/Asteroid.gd
@@ -1,6 +1,16 @@
 extends RigidBody2D
 
 
-func _ready():
-	pass
+func explode():
+	for i in range(5):
+		var vel = Vector2(0, rand_range(100, 500)).rotated(rand_range(0, PI*2))
+		get_parent().get_parent().add(2, position, vel)
+	get_parent().get_parent().remove_instance(1, self)
 
+
+func _on_Asteroid_body_entered(body):
+	if body.name == "Player":
+		var shipass = body.get_node("Ship_assembler")
+		if !shipass.moduleFlags[3]:
+			body.game_over()
+		explode()
--- ld-47/scripts/Target.gd
+++ ld-47/scripts/Target.gd
@@ -1,11 +1,5 @@
 extends Sprite
 
-var asteroid
-
-func _ready():
-	asteroid = preload("res://scenes/Asteroid.tscn")
-	
-	
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
@@ -20,7 +14,5 @@ func explode():
 	for i in range(3):
 		var vel = Vector2(0, rand_range(100, 500)).rotated(rand_range(0, PI*2))
 		get_parent().get_parent().add(1, position, vel)
-		#get_parent().add_child(next)
 	get_parent().get_parent().remove_instance(0, self)
-	#visible = false
-	#$Area2D.queue_free()
+
