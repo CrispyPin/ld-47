f0e208fc579a19affb2845a4f3f72af904a54503
CrispyPin <crispin@tasa.se>
2020-10-04 16:19:32 +0200

    asteroids explode on impact with shield

--- ld-47/scripts/Target.gd
+++ ld-47/scripts/Target.gd
@@ -11,9 +11,17 @@ func _on_Area2D_body_entered(body):
 	if body.name == "Player":
 		var shipass = body.get_node("Ship_assembler")
 		if shipass.moduleFlags[3]:
+			shipass.setModuleTop(3, false)
 			explode()
 		else:
 			body.game_over()
 
 func explode():
-	add_child(asteroid.instance())
+	for i in range(3):
+		var next = asteroid.instance()
+		next.position = position
+		next.linear_velocity = Vector2(0, rand_range(100, 500)).rotated(rand_range(0, PI*2))
+		get_parent().add_child(next)
+	get_parent().targets.remove(get_parent().targets.find(self))
+	visible = false
+	$Area2D.queue_free()
