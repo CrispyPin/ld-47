eb01632a14288dfaae0faf6bef30281eab3e5a65
CrispyPin <crispin@tasa.se>
2020-10-04 11:45:28 +0200

    add gradient to trails

--- ld-47/scripts/Trail.gd
+++ ld-47/scripts/Trail.gd
@@ -1,15 +1,20 @@
 extends Line2D
 
-
+var l = 50
 var point
 var target_pointer
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
 	target_pointer = get_node("../..")
 
 func _process(delta):
 	point = target_pointer.global_position
 	add_point(point)
-	while get_point_count()>100:
+	while get_point_count() > l:
 		remove_point(0)
 
+
+
+func _on_TrailEmitter_tree_exiting():
+	clear_points()
