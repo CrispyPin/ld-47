e2caede43436d0182fd9153dd3187e2642b9b536
eriki <erikiscoolqwe@gmail.com>
2020-10-04 20:56:58 +0200

    MORE cannon stuff

--- ld-47/scripts/Cannon.gd
+++ ld-47/scripts/Cannon.gd
@@ -2,24 +2,38 @@ extends Sprite
 
 
 var targetSpawner
-var ClosestAsteroid
+var TargetAsteroid
 func _ready():
 	targetSpawner = get_node("/root/GameScene/TargetSpawner")
 
 func NewTarget():
 	var Asteroids = targetSpawner.asteroids
-	ClosestAsteroid = null
+	TargetAsteroid = null
 	var SmallestDist = global_position.distance_to(Asteroids[0].global_position)
 	for Asteroid in Asteroids:
 		var dist = global_position.distance_to(Asteroid.global_position)
 		if dist <= SmallestDist and dist < 400:
-			ClosestAsteroid = Asteroid
+			TargetAsteroid = Asteroid
 			SmallestDist = dist
 
-var a = true
+var bTargeting = false
+var fShootWait = 0
+
 func _physics_process(delta):
-	if (true):
-		#a = false
+	if (!bTargeting):
 		NewTarget()
-		if (ClosestAsteroid):
-			ClosestAsteroid.explode()
+		if (TargetAsteroid):
+			bTargeting = true
+			fShootWait = 3
+			
+		get_child(0).rotation = rotation + PI
+	else:
+		
+		get_child(0).rotation = get_angle_to(TargetAsteroid.global_position) + PI/2
+		
+		fShootWait-=delta
+		if fShootWait < 0:
+			if TargetAsteroid:
+				TargetAsteroid.explode()
+			bTargeting = false
+
