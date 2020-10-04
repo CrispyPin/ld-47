ce72010fb4ed2b314d268b6a24eee73c8f4251fc
CrispyPin <crispin@tasa.se>
2020-10-04 13:51:28 +0200

    create asteroid on impact

---
+++ ld-47/scripts/Target.gd
@@ -0,0 +1,19 @@
+extends Sprite
+
+var asteroid
+
+func _ready():
+	asteroid = preload("res://scenes/Asteroid.tscn")
+	
+	
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		var shipass = body.get_node("Ship_assembler")
+		if true:#shipass.moduleFlags[3]:
+			explode()
+		else:
+			body.game_over()
+
+func explode():
+	add_child(asteroid.instance())
