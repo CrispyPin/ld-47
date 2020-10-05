f46a7352e8cab6eeb480c83abf026f29978c6df4
CrispyPin <CrispyPin@users.noreply.github.com>
2020-10-05 15:56:16 +0200

    add game over text, add input maps

--- ld-47/scripts/DebugMenu.gd
+++ ld-47/scripts/DebugMenu.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_end"):
+	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
